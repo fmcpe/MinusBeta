c596fcb510f47e51bbf5e5959e6e50fa24b77118
LongAthelstan <121792617+longathelstan@users.noreply.github.com>
2024-07-15 17:59:08 +0700

    better logic

--- src/main/java/net/minusmc/minusbounce/features/module/modules/world/Scaffold.kt
+++ src/main/java/net/minusmc/minusbounce/features/module/modules/world/Scaffold.kt
@@ -106,7 +106,7 @@ class Scaffold: Module(){
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
         // jumpAutomatically only godbridge
-         if (MovementUtils.isMoving() && mc.thePlayer.onGround && !mc.thePlayer.isSneaking && !mc.gameSettings.keyBindSneak.isKeyDown && !mc.gameSettings.keyBindJump.isKeyDown &&
+         if (jumpAutomatically.get() && MovementUtils.isMoving() && mc.thePlayer.onGround && !mc.thePlayer.isSneaking && !mc.gameSettings.keyBindSneak.isKeyDown && !mc.gameSettings.keyBindJump.isKeyDown &&
                 mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.entityBoundingBox
                         .offset(0.0, -0.5, 0.0).expand(-0.001, 0.0, -0.001)).isEmpty())
             mc.thePlayer.jump()
